52d46263a69710b8e34a77617a3fb3b49d4040bd
Paul Bailey <paul@neutron.studio>
2021-03-27 10:30:49 -0500

    function intro

--- js/js-intro.js
+++ js/js-intro.js
@@ -1,2 +1,43 @@
+var out1;
+var out2;
+const PI = 3.14;
 
-console.log("Hello Flex Class");
+function hello (name) {
+  var output;
+
+  output = 'Hello ' + name;
+  console.log(output);
+
+  return output;
+}
+
+function hello_nothing () {
+  console.log("Hello Nothing");
+}
+
+function hello2() {
+  var output = "hello2";
+  return output;
+}
+
+function hello_all () {
+  for (var i=0; i < arguments.length; i++) {
+    console.log('Hello ' + arguments[i]);
+  }
+}
+
+out1 = hello("Paul");
+out2 = hello("Class");
+
+console.log(out1, out2);
+
+out2 = "Overwritten";
+
+hello_nothing();
+hello_nothing();
+
+console.log(out1, out2);
+
+hello_all();
+console.log("-------");
+hello_all("Argument", "Arg2", "Arg3");
